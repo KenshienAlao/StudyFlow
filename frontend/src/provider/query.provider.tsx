cede155d4fb9663f0b7f2aed7d1315375dc71e11
kenshien <kenshienworkacc@gmail.com>
2026-06-25 12:24:44 +0800

    chore: reformat the code

--- frontend/src/provider/query.provider.tsx
+++ frontend/src/provider/query.provider.tsx
@@ -1,22 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { useState } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-    const [queryClient] = useState(() => new QueryClient({
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
         defaultOptions: {
-            queries: {
-                staleTime: 60 * 1000 * 10,
-                gcTime: 1000 * 60 * 10,
-                retry: 1,
-                refetchOnMount: false,
-                refetchOnWindowFocus: false,
-            }
-        }
-    }))
-    
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    )
+          queries: {
+            staleTime: 60 * 1000 * 10,
+            gcTime: 1000 * 60 * 10,
+            retry: 1,
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
